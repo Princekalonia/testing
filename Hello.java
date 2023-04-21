7842f1e1d1dac991898cb416a9d08a3acaa5c79c
Princekalonia <princekalonia@gmail.com>
2023-04-21 10:52:38 +0530

    2nd commit

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Today I will Be in A Meeting");
+            int a=12;
+            System.out.println(a*a);
 	}
 
 }
